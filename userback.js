f94dea28ff6c6393105c86eb02551cc3e68dd273
Chanhyeok Lee <gkausehlsekgo@gmail.com>
2026-02-26 12:04:18 +0900

    feat: integrate Userback feedback widget site-wide

---
+++ userback.js
@@ -0,0 +1,8 @@
+window.Userback = window.Userback || {};
+window.Userback.access_token = "A-D53x5lCG9pDrALithTFD6bybk";
+(function (d) {
+    var s = d.createElement('script');
+    s.async = true;
+    s.src = 'https://static.userback.io/widget/v1.js';
+    (d.head || d.body).appendChild(s);
+})(document);
